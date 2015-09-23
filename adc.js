c224384f18ba2e09c8e6905863d19d80f37f683f
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-23 20:59:32 +0100

    add interval

--- adc.js
+++ adc.js
@@ -6,15 +6,14 @@ console.log("starting ADC script...")
   // no pinmode is required for analogRead as those pins are dedicated.
 var pin = 'P9_33'; //the pin to operate on
 
-var readADC = function(callback) {
+var readADC = function() {
   b.analogRead(pin, function(err, value) {
     if (err) {
       console.error(err.message);
-      callback(err);
+      return;
     }
     console.log('Analog Value: ' + value); // value is floating point number between 0 and 1.
     console.log('Voltage: ' + value * 1.8 + ' V')
-    callback(value);
   });
 };
 
